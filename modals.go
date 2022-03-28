f33755e5837542529be2df5d7be70775e1bfa78d
YashRaj <yashraj0207@outlook.com>
2022-03-28 12:14:19 +0530

    I-0 : Modals : changed json name for userID

--- modals.go
+++ modals.go
@@ -3,7 +3,7 @@ package gomongoauth
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
 	Name     string             `json:"name" validate:"required,min=2,max=100"`
 	Email    string             `json:"email" validate:"required,email"`
 	Password string             `json:"password" validate:"required,min=6"`
